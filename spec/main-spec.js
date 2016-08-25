071401c9f24fffa40f6b1a151416d0169757ed00
Steel Brain <i@steelbrain.me>
2016-08-24 19:28:30 -0700

    :white_check_mark: Upgrade specs to latest version

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -4,22 +4,17 @@ import Path from 'path'
 import npmPath from '../'
 
 describe('npm-path', function() {
-  function join(...params) {
-    return params.join(':')
-  }
-
   const DIR = Path.join(__dirname, 'fixtures')
   const DEEP_DIR = Path.join(DIR, 'test')
   const BIN_DIR = Path.join(DIR, 'node_modules', '.bin')
   const DEEP_BIN_DIR = Path.join(DEEP_DIR, 'node_modules', '.bin')
-  const ROOT_BIN = Path.normalize(Path.join(__dirname, '..', 'node_modules', '.bin'))
 
   describe('::getPATH', function() {
     it('works', function() {
-      expect(npmPath(DIR)).toBe(join(BIN_DIR, ROOT_BIN))
+      expect(npmPath(DIR)).toBe(BIN_DIR)
     })
     it('works deep', function() {
-      expect(npmPath(DEEP_DIR)).toBe(join(DEEP_BIN_DIR, BIN_DIR, ROOT_BIN))
+      expect(npmPath(DEEP_DIR)).toBe(DEEP_BIN_DIR)
     })
   })
 
@@ -27,14 +22,14 @@ describe('npm-path', function() {
     it('works', function() {
       waitsForPromise(function() {
         return npmPath.async(DIR).then(function(result) {
-          expect(result).toBe(join(BIN_DIR, ROOT_BIN))
+          expect(result).toBe(BIN_DIR)
         })
       })
     })
     it('works deep', function() {
       waitsForPromise(function() {
         return npmPath.async(DEEP_DIR).then(function(result) {
-          expect(result).toBe(join(DEEP_BIN_DIR, BIN_DIR, ROOT_BIN))
+          expect(result).toBe(DEEP_BIN_DIR)
         })
       })
     })
